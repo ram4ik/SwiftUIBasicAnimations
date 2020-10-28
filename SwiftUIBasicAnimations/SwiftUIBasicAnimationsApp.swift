f1bcc1517ea8cc857e96d03e9ba7a54d2ee219a7
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-10-28 15:46:34 +0200

    Initial Commit

---
+++ SwiftUIBasicAnimations/SwiftUIBasicAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicAnimationsApp.swift
+//  SwiftUIBasicAnimations
+//
+//  Created by Ramill Ibragimov on 28.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
